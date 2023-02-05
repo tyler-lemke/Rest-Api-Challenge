c35f8c70d4e591da04e44a2ab1702b31d345ccfd
tylerslemke <tyleris@gmail.com>
2023-02-04 18:35:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rest-Api-Challenge
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-8bpr4c)
---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+// Use the axios documentation for reference
+
+// Part 1
+// Visit swapi.dev
+// grab films data from the film endpoint
+// console.log  out the film data you grabbed
+
+// Part 2
+// Now that you have the film data
